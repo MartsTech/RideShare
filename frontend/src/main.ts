3352b71ae8c2d3080e4566a12ae041d0a50c6868
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2023-04-15 17:56:57 +0300

    Clean frontend

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,7 @@
-import { createApp } from 'vue'
+import App from '@app/App.vue'
+import router from '@app/router'
 import { createPinia } from 'pinia'
-
-import App from './App.vue'
-import router from './router'
-
-import './assets/main.css'
+import { createApp } from 'vue'
 
 const app = createApp(App)
 
